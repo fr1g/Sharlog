6e2a23971d70a10579033f1b53db35cd3d6e4904
Feiron <53085628+fr1g@users.noreply.github.com>
2023-09-18 21:44:43 +0800

    -

--- Sharlog/Program.cs
+++ Sharlog/Program.cs
@@ -1,58 +1,24 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using MudBlazor.Services;
-using Sharlog.Areas.Identity;
-using Sharlog.Data;
+﻿using System;
+using System.Runtime.Loader;
+using Sharlog;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-builder.Services.AddSingleton<WeatherForecastService>();
-
-builder.Services.AddMudServices();
-
-// builder.Services.AddSingleton(IHostEnvironment);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+AssemblyLoadContext.Default.Resolving += (context, name) =>
 {
-    app.UseMigrationsEndPoint();
-}
-else
+    // Replace "path_to_your_dlls" with the actual path to your DLLs
+    var assemblyPath = $"Depend/{name.Name}.dll";
+    if (System.IO.File.Exists(assemblyPath))
+    {
+        return context.LoadFromAssemblyPath(assemblyPath);
+    }
+    return null;
+};
+
+var localDir = new DirectoryInfo("./Sharlog");
+if (!localDir.Exists)
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    localDir.Create();
+    // continue creating folders else...
+
 }
 
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-//app.Services.CreateScope();
-
-app.MapControllers();
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
-
+Initialize.X(args);
